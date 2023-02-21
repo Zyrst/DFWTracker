8235bb324f27c5cbac4d0702d3d8850dd538cf63
Zyrst <zajreg@gmail.com>
2023-02-21 21:20:36 +0100

    Make reusable componets

--- main.js
+++ main.js
@@ -15,3 +15,23 @@ function resetAll(){
 function persist(event){
     localStorage.setItem(event.id, event.value);
 }
+
+class CleanAndPress extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+        <input type="number" id="${this.id}" placeholder="Clean & Press" class="form-control" onchange="persist(this)"/>
+        `
+    }
+}
+
+class Squat extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+        <input type="number" id="${this.id}" placeholder="Squat" class="form-control" onchange="persist(this)"/>
+        `
+    }
+}
+
+
+customElements.define('clean-press', CleanAndPress);
+customElements.define('squat-b', Squat);
